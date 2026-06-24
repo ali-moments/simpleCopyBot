9951c807316b15e83a157ee5cfe4c5e212b819ec
ali-moments <a.ham2334@gmail.com>
2026-06-24 15:16:46 +0000

    Add deque for tracking seen messages and commands to toggle bot activity

--- listener.py
+++ listener.py
@@ -1,7 +1,9 @@
+from collections import deque
 from telethon import events
 from parsers.monk import parse as monk_parse
 from parsers.mahee import parse as mahee_parse
 from parsers.gcr import parse as gcr_parse
+import main
 
 CHANNELS = {
     # Test Channel for mock data
@@ -12,14 +14,24 @@ CHANNELS = {
     -1001895114921: gcr_parse,
 }
 
+seen_ids = deque(maxlen=100)
+
 
 def register(reader, sender):
     @reader.on(events.NewMessage(chats=list(CHANNELS.keys())))
     async def handler(event):
+        if not main.is_active:
+            return
+        msg_id = (event.chat_id, event.message.id)
+        if msg_id in seen_ids:
+            return
+        seen_ids.append(msg_id)
+
         chat_id = event.chat_id
         parser = CHANNELS.get(chat_id)
         if not parser:
             return
+
         # print(repr(event.message.text))
         signal = parser(event.message.text.replace("**", ""))
         if signal:
--- main.py
+++ main.py
@@ -6,44 +6,45 @@ import listener
 reader = TelegramClient("reader_session", APP_ID, APP_HASH)
 sender = TelegramClient("sender_session", PREMIUM_APP_ID, PREMIUM_APP_HASH)
 
+is_active = True
+
 message = """
 DIAMOND
 <tg-emoji emoji-id="5039816072253932764">💎</tg-emoji>
-
 LONG_ARROW
 <tg-emoji emoji-id="5449683594425410231">🔼</tg-emoji>
-
 SHORT_ARROW
 <tg-emoji emoji-id="5447183459602669338">🔽</tg-emoji>
-
 ENTRY_EMOJI
 <tg-emoji emoji-id="5443127283898405358">📥</tg-emoji>
-
 LIGHTNING
 <tg-emoji emoji-id="4956233646441760046">🌩</tg-emoji>
-
 STOPLOSS_EMOJI
 <tg-emoji emoji-id="4958621433509970793">🔖</tg-emoji>
-
 WARNING
 <tg-emoji emoji-id="5420323339723881652">⚠️</tg-emoji>
-
 CROWN
 <tg-emoji emoji-id="5843667185074969423">👑</tg-emoji>
-
 """
+
 
 @sender.on(events.NewMessage(pattern='/start'))
 async def start_handler(event):
-    await event.reply(
-        message,
-        parse_mode="html"
-    )
-    # await sender.send_message(
-    #     TARGET_CHANNEL,
-    #     '<tg-emoji emoji-id="5039816072253932764">💎</tg-emoji> test',
-    #     parse_mode="html"
-    # )
+    await event.reply(message, parse_mode="html")
+
+
+@sender.on(events.NewMessage(pattern='/on'))
+async def on_handler(event):
+    global is_active
+    is_active = True
+    await event.reply("✅ Bot is ON")
+
+
+@sender.on(events.NewMessage(pattern='/off'))
+async def off_handler(event):
+    global is_active
+    is_active = False
+    await event.reply("🛑 Bot is OFF")
 
 
 async def main():
